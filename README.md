dfbb55e9674af191b5abfbf65650d0474b8dd8a2
Gymnophoria <buddercreeperman@gmail.com>
2019-07-25 03:45:01 -0700

    move server events to readme
    soon to move the rest over too, lol

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Arthur-IPC
 The IPC (inter-process communication) module for Arthur. Runs an IPC server using node-ipc to allow communication between the [Arthur Bot](https://github.com/Gymnophoria/Arthur) and the [Arthur Website](https://github.com/Gymnophoria/Arthur-Website).
+
+## Documentation
+###Server Events
+Events that the server receives from clients. Each event has a JSON object as data, with keys defined below.
+#### hello
+Allows server to cache socket and start any repetitive data polls (intervals) (e.g. sending cached stats to website)
+ - `id`, string: The id of the client connecting, should be either `website` or `bot`.
+ - `intervals`, array\<string>: Types of data to be sent on an interval. Sends all data associated with type, regardless of ID, so.. be careful.
+ 
+#### data
+Sent with data to be cached, likely sent on an interval.
+ - `type`, **string**: The type of the data. One of the data types defined in a section below.
+ - `data`, **object**: The data to be cached.
+ - `time`, **number**: The time, in seconds, for the data to be kept in the cache. A value less than 0 means that the data should not be cached, and no value or 0 means that the data should be cached forever.
+
+#### get
+An event that requests data from the cache or the requested resource.
+ - `from`, **string**: Where to get the data from; `bot` or `website`.
+ - `type`, **string**: The data type. Types defined below.
+ - `request`, **number**: The request number, provided by the client so it can differentiate `get` requests.
+ - `id`, **string** *(optional)*: The ID for the type of data requested. May be necessary to get data (e.g. guild ID).
+ - `fresh`, **boolean** *(optional)*: Whether or not to get fresh data; that is, data that is not cached by the server.
+ 
+#### response
+A response (to a `get` event, client-side) from a client with data.
+ - `request`, **number**: The request number that the server provided with the get request.
+ - `data`, **object**: The data requested.
+ - `time`, **number**: Same as in `data`; time for data to be cached, in seconds.
+
+    **OR**
+
+ - `request`, **number**: The request number that the server provided with the get request.
+ - `error`, **string**|`true`: The error the client encountered while attempting to get the data.
--- index.js
+++ index.js
@@ -7,45 +7,21 @@ ipc.config.id = 'ipcServer';
 ipc.config.retry = 1500;
 ipc.config.maxConnections = 2;
 
-/* Types *
+/* Types * 
  * guild: A partial discord guild object, with relevant info
  * music: A guild's music object
  * stats*: A stats object (yet to be defined) with system stats, command stats, music stats, and more
  * locale: A locale file in its entirety
  * commands*: Client commands' `command.options` object, such that cooldowns, category, etc. can be viewed
  *           on website.
- * userInfo: A user's preferences, such as locale, as well as their XP info
+ * userInfo: A partial discord user object plus user's preferences, such as locale, as well as their XP info
  * guildXP: A guild's XP info
  * a * indicates that the type does not have a id, and will thus use '0' as the id.
- * 
- * Server Events *
- * hello: object with { id: 'bot' | 'website' , intervals: [] }, allows server to cache socket and start any 
- *        repetitive data polls (intervals) (e.g. sending cache data to website)
- *        `intervals` is an array with all types of data to be sent on an interval. Sends all
- *              data associated with type, so like, gotta be careful.
- *        
- * data: Sent with data for cache to update with. { type: 'guild|music|stats|etc.', data: { ... }, time: 43, id: '34 }
- *              `time` is the time, in seconds, for the data to be cached locally, less than 0 means shouldn't be
- *                      cached, 0 or no time means cache forever.
- * 
- * get: { from: 'bot|website', type: 'guild|music|stats|locale', id: '392394923', request: 3 }
- *      Gets requested data either from cache or from requested resource
- *      `request` is a request ID from requester
- *      `id` is optional but may be necessary for datatype; e.g. guild ID or locale code
- *              Responds with a `response` event emited
- *      
- * response: Response from bot or website with data. { request: 4, data: { ... }, time: 3 }
- *           `request` is a request ID given from the server,
- *           `time` is same as above
- *           
- * musicUpdate: From website to bot only, changes bot music in some way. Updates cache and sends same event to bot.
- *              `type`: one of the music types (togglePausePlay, addToQueue, stop, skip, likeToggle, loop, remove, shuffle, move)
- *              `guild`: the ID of the guild where music is being played
- *              `user`: the user ID of the person doing the action
  *           
  *           
  * Client Events *
  * data: { type: 'same', data: { ... }, time: 39, id: '23' } - Data given from a interval, for example
+ * 
  * 
  * get: { type: 'same', id: 'same', request: 93 } - Get data for server.
  *             Responds with a `respond` event.
@@ -54,7 +30,7 @@ ipc.config.maxConnections = 2;
  * 
  * 
  * Bot Only *
- * musicUpdate: responds to musicUpdate event from server, documented in client events section
+ * musicUpdate: responds to musicUpdate event from server, documented in server events section
  */
 
 let cache = new Cache();
@@ -143,7 +119,7 @@ function waitForReconnect(data, socket, i = 0) {
 	if (i > 10) return sendError(socket, data.request, `Could not connect to ${data.from}.`);
 	
 	setTimeout(() => {
-		waitForReconnect(data, socket, ++i);
+		waitForReconnect(data, socket, i + 1);
 	}, 500);
 }
 
